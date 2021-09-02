a2da2495c8645198ada68a0ca4b528502733901a
Melchor Gabriel Lopez Castro <melchorgabriel@gmail.com>
2021-09-02 07:50:10 -0600

    Clase 02092021

---
+++ Grupo52/Grupo52.Api/Controllers/EquiposController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Grupo52.Api.Models;
+using System.Collections.Generic;
+
+namespace Grupo52.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EquiposController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Equipo> Equipos = new List<Equipo>();
+/*
+            var equipo = new Equipo
+            {
+                IdEquipo=1,
+                Nombre="Pumas",
+                Logotipo="logopumas.jpg",
+                Ciudad="Monterrey"
+            };
+*/
+            Equipos.Add(new Equipo { IdEquipo = 1,Nombre = "Pumas",Logotipo = "logopumas.jpg",Ciudad = "Monterrey"});
+            Equipos.Add(new Equipo { IdEquipo = 2, Nombre = "Pumas", Logotipo = "logopumas.jpg", Ciudad = "Monterrey" });
+            Equipos.Add(new Equipo { IdEquipo = 3, Nombre = "Pumas", Logotipo = "logopumas.jpg", Ciudad = "Monterrey" });
+
+
+            return Ok(Equipos);
+        }
+    }
+}
---
+++ Grupo52/Grupo52.Api/Models/Equipo.cs
@@ -0,0 +1,13 @@
+﻿namespace Grupo52.Api.Models
+{
+    public class Equipo
+    {
+        public int IdEquipo { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Logotipo { get; set; }
+
+        public string Ciudad { get; set; }
+    }
+}
