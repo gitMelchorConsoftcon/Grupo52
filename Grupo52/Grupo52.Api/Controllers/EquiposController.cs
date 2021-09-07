f2e70b22351619b33ffa6a2e28faf375b6450040
Melchor Gabriel Lopez Castro <melchorgabriel@gmail.com>
2021-09-07 07:31:19 -0600

    Update EquiposController.cs

--- Grupo52/Grupo52.Api/Controllers/EquiposController.cs
+++ Grupo52/Grupo52.Api/Controllers/EquiposController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Grupo52.Api.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Grupo52.Api.Controllers
 {
@@ -8,26 +9,81 @@ namespace Grupo52.Api.Controllers
     [ApiController]
     public class EquiposController : ControllerBase
     {
-
-        [HttpGet]
-        public IActionResult Get()
+        List<Equipo> Equipos = new List<Equipo>();
+        public EquiposController()
         {
-            List<Equipo> Equipos = new List<Equipo>();
-/*
-            var equipo = new Equipo
-            {
-                IdEquipo=1,
-                Nombre="Pumas",
-                Logotipo="logopumas.jpg",
-                Ciudad="Monterrey"
-            };
-*/
-            Equipos.Add(new Equipo { IdEquipo = 1,Nombre = "Pumas",Logotipo = "logopumas.jpg",Ciudad = "Monterrey"});
+            
+            Equipos.Add(new Equipo { IdEquipo = 1, Nombre = "Pumas", Logotipo = "logopumas.jpg", Ciudad = "Monterrey" });
             Equipos.Add(new Equipo { IdEquipo = 2, Nombre = "Pumas", Logotipo = "logopumas.jpg", Ciudad = "Monterrey" });
             Equipos.Add(new Equipo { IdEquipo = 3, Nombre = "Pumas", Logotipo = "logopumas.jpg", Ciudad = "Monterrey" });
+            Equipos.Add(new Equipo { IdEquipo = 4, Nombre = "Pumas", Logotipo = "logopumas.jpg", Ciudad = "Monterrey" });
+        }
 
+
+        [HttpGet] // Sirve para obtener informacion del servidor
+        public IActionResult Get()
+        {      
+            return Ok(Equipos);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetEquipo(int id)
+        {
+            var equipo = Equipos.Where(x=> x.IdEquipo==id).FirstOrDefault();
+
+            if (equipo == null)
+                return NotFound();
+
+            return Ok(equipo);
+        }
+
+        [HttpPost]  // sirve para guardar informacion
+        public IActionResult GuardarEquipo (Equipo equipo)
+        {
+
+            var eq = new Equipo
+            {
+                IdEquipo=equipo.IdEquipo,
+                Nombre=equipo.Nombre,
+                Logotipo=equipo.Logotipo,
+                Ciudad=equipo.Ciudad
+            };
+
+            Equipos.Add(eq);
+            return Ok(Equipos);
+        }
+
+
+        [HttpPut] // sirve para modificar informacion
+        [Route("{id}")]         
+        public IActionResult ModificarEquipo(int id ,Equipo equipo)
+        {
+
+            var modificar = Equipos.Where(x => x.IdEquipo == id).FirstOrDefault();
+
+            if (modificar!= null)
+            {
+                modificar.Nombre = equipo.Nombre;
+                modificar.Ciudad = equipo.Ciudad;
+                modificar.Logotipo = equipo.Logotipo;
+                
+            }
+            return Ok(modificar);
+        }
+
+
+        [HttpDelete]   // sirver para borrar
+        [Route("{id}")]              
+        public IActionResult BorrarEquipo (int id)
+        {
+
+            var borrar = Equipos.Where(x => x.IdEquipo == id).FirstOrDefault();
+
+            Equipos.Remove(borrar);
 
             return Ok(Equipos);
         }
+ 
     }
 }
