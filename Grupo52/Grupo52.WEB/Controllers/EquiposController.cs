fbc28724baa5d99b401a043d6adf2addcf37966b
Melchor Gabriel Lopez Castro <melchorgabriel@gmail.com>
2021-11-08 07:45:08 -0700

    web

---
+++ Grupo52/Grupo52.WEB/Controllers/EquiposController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Grupo52.WEB.Controllers
+{
+    public class EquiposController : Controller
+    {
+       
+       
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+    }
+}
